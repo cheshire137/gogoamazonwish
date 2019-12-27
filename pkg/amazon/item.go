e542aa92a503475da14b6c6b4259dd3668ede4ae
Sarah Vessels <cheshire137@gmail.com>
2019-12-27 12:14:46 -0600

    Handle missing values in Item

--- pkg/amazon/item.go
+++ pkg/amazon/item.go
@@ -13,6 +13,27 @@ type Item struct {
 }
 
 func (i *Item) String() string {
-	return fmt.Sprintf("%s %s\n\tAdded: %s\n\t<%s>", i.Name, i.Price, i.DateAdded,
-		i.DirectURL)
+	if i.DateAdded != "" && i.Price != "" && i.Name != "" && i.DirectURL != "" {
+		return fmt.Sprintf("%s %s\n\tAdded: %s\n\t<%s>", i.Name, i.Price, i.DateAdded,
+			i.DirectURL)
+	}
+	if i.Price != "" && i.Name != "" && i.DirectURL != "" {
+		return fmt.Sprintf("%s %s\n\t<%s>", i.Name, i.Price, i.DirectURL)
+	}
+	if i.DateAdded != "" && i.Name != "" && i.DirectURL != "" {
+		return fmt.Sprintf("%s\n\tAdded: %s\n\t<%s>", i.Name, i.DateAdded, i.DirectURL)
+	}
+	if i.Name != "" && i.DirectURL != "" {
+		return fmt.Sprintf("%s\n\t<%s>", i.Name, i.DirectURL)
+	}
+	if i.Name != "" {
+		return i.Name
+	}
+	if i.DirectURL != "" {
+		return i.DirectURL
+	}
+	if i.ID != "" {
+		return i.ID
+	}
+	return "Wishlist item"
 }
