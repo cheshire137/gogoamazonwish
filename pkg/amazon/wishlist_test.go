7ccb709b74beed9fe1d78ff3e689527e4c3c359b
Sarah Vessels <cheshire137@gmail.com>
2019-12-29 17:34:15 -0600

    Test NewWishlistFromIDAtDomain

--- pkg/amazon/wishlist_test.go
+++ pkg/amazon/wishlist_test.go
@@ -1,6 +1,9 @@
 package amazon
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -8,16 +11,47 @@ import (
 
 func TestNewWishlistFromID(t *testing.T) {
 	id := "123abc"
-
 	wishlist, err := NewWishlistFromID(id)
-
 	require.NoError(t, err)
+
 	urls := wishlist.URLs()
 	require.NotEmpty(t, urls)
 
 	for _, url := range urls {
-		require.Contains(t, url, "amazon.com")
+		require.Contains(t, url, DefaultAmazonDomain)
 		require.Contains(t, url, id)
 		require.Contains(t, url, "wishlist")
 	}
+}
+
+func TestNewWishlistFromIDAtDomain(t *testing.T) {
+	id := "123abc"
+	ts := newTestServer(id)
+	defer ts.Close()
+	fmt.Println("test server", ts.URL)
+
+	wishlist, err := NewWishlistFromIDAtDomain(id, ts.URL)
+	require.NoError(t, err)
+
+	urls := wishlist.URLs()
+	require.NotEmpty(t, urls)
+
+	for _, url := range urls {
+		require.Contains(t, url, ts.URL)
+		require.Contains(t, url, id)
+		require.Contains(t, url, "wishlist")
+	}
+}
+
+func newTestServer(wishlistID string) *httptest.Server {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hz/wishlist/ls/"+wishlistID, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte(`<!DOCTYPE html><html><head></head>
+<body></body></html>
+		`))
+	})
+
+	return httptest.NewServer(mux)
 }
